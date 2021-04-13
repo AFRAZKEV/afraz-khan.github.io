428e245c5bcaa7b3be1fdfbf18d4ab79db04f08a
afraz-khan <ah.afrazkhan@gmail.com>
2021-04-13 20:41:33 +0500

    Responsivness added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import HiFaceImage from './hi-face.png';
 import './App.css';
-import Header from './components/Header';
 
 function App() {
   return (
-      <Header/>
+    <div>
+      <div className='container' >
+      <img src={HiFaceImage} className="img-fluid" alt="..."></img>
+    </div>
+    <div className='container-fluid name-container' >
+    <p className='text-sm-left name' >my_name = 'Afraz Khan'</p>
+    </div>
+    </div>
   );
 }
 
