5add4ea6fd4a74e846f74337eb9a804b103845a3
afraz-khan <ah.afrazkhan@gmail.com>
2021-05-12 03:48:30 +0500

    JSON in HTML links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <footer className='page-footer font-small mdb-color pt-4' >
-            <div className='container' >
+            <div className='container-flued' >
                 <div className='Row text-center' >
                     @2021 afraz-khan.github.io
                 </div>
@@ -11,5 +11,15 @@ function Footer(){
         </footer>
     );
 }
+
+// function Footer(){
+//     return(
+//         <div className="row justify-content-center mx-auto footer" >
+//             <div className="col-lg-4" >asd</div>
+//             <div className="col-lg-4" >asd</div>
+//             <div className="col-lg-4" >asd</div>
+//         </div>
+//     );
+// }
 
 export default Footer;
