42f7beed6aa535613978d91120377bce584e2d6b
afraz-khan <ah.afrazkhan@gmail.com>
2021-05-12 20:52:45 +0500

    Adding footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,35 +7,29 @@ import github from '../images/github.png'
 function Footer(){
     return(
         <footer >
-            <div className='row' >
-                <div className='col-lg-6' >
-                        @2021 afraz-khan.github.io
-                </div>
-                <div className='col-lg-6' >
-                    <ul >
-                        <li >
-                            <a href='https://www.instagram.com/mrafrazkhan/' >
-                                <img className='social-icon' src={instagram} />
-                            </a>
-                        </li>
-                        <li >
-                            <a href='https://twitter.com/MrAfrazKhan' >
-                                <img className='social-icon' src={twitter} />
-                            </a>
-                        </li>
-                        <li >
-                            <a href='https://www.linkedin.com/in/afraz-khan/' >
-                                <img className='social-icon' src={linkedin} />
-                            </a>
-                        </li>
-                        <li >
-                            <a href='https://github.com/afraz-khan' >
-                                <img className='social-icon' src={github} />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            @2021 afraz-khan.github.io
+            <ul >
+                <li >
+                    <a href='https://www.instagram.com/mrafrazkhan/' >
+                        <img className='social-icon' src={instagram} />
+                    </a>
+                </li>
+                <li >
+                    <a href='https://twitter.com/MrAfrazKhan' >
+                        <img className='social-icon' src={twitter} />
+                    </a>
+                </li>
+                <li >
+                    <a href='https://www.linkedin.com/in/afraz-khan/' >
+                        <img className='social-icon' src={linkedin} />
+                    </a>
+                </li>
+                <li >
+                    <a href='https://github.com/afraz-khan' >
+                        <img className='social-icon' src={github} />
+                    </a>
+                </li>
+            </ul>
         </footer>
     );
 }
